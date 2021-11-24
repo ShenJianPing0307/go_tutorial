08994a01e0a96a8586fb053de9a1bd72794a31d8
ShenJianPing0307 <610502705@qq.com>
2021-11-24 19:39:14 +0800

    异常处理

---
+++ day08/02/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	now := time.Now()
+
+	// 通过now获取年月日、时分秒
+	fmt.Printf("年=%v\n", now.Year())
+	fmt.Printf("月=%v\n", now.Month())
+	fmt.Printf("日=%v\n", now.Day())
+	fmt.Printf("时=%v\n", now.Hour())
+	fmt.Printf("分=%v\n", now.Minute())
+	fmt.Printf("秒=%v\n", now.Second())
+
+}
+/*
+年=2021
+月=November
+日=21      
+时=21
+分=58
+秒=45
+*/
---
+++ day08/04/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	now := time.Now()
+
+	// 通过now获取年月日、时分秒
+	date := fmt.Sprintf("年月日%d-%d-%d %d:%d:%d", now.Year(),
+	now.Month(),now.Day(),now.Hour(),
+	now.Minute(),now.Second()) 
+
+	fmt.Printf("date=%v", date) // date=年月日2021-11-21 22:7:43
+
+}
