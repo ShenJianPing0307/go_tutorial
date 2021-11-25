90939cb74a87a467414c32581bcb87afc7f8e200
ShenJianPing0307 <610502705@qq.com>
2021-11-25 22:32:18 +0800

    数组应用案例

---
+++ day09/arrayDemo5/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+ )
+
+func main() {
+
+	var intArr [5]int
+
+	len := len(intArr)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < len; i++ {
+		intArr[i] = rand.Intn(100) // 生成 大于等于0，小于100的5个数
+	}
+
+	fmt.Println("交换前：", intArr)
+
+	// 进行交换
+	temp := 0
+	for i := 0; i < len / 2; i++ {
+		temp = intArr[len - 1 - i]
+		intArr[len - 1 - i] = intArr[i]
+		intArr[i] =temp
+	}
+	fmt.Println("交换后：", intArr)
+	
+}
+/*
+交换前： [42 43 95 0 67]
+交换后： [67 0 95 43 42]
+*/
