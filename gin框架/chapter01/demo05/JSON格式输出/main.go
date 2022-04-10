a8b7013fb2196150c438b1260fa2da6badc09ccb
ShenJianPing0307 <610502705@qq.com>
2022-04-10 22:11:46 +0800

    Gin框架系列之不同格式数据输出

---
+++ gin框架/chapter01/demo05/JSON格式输出/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Index(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"code":    2001,
+		"message": "success",
+	})
+
+}
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/index", Index)
+
+	router.Run(":8080")
+
+}
